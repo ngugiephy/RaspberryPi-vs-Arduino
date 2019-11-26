4b5b01a19cd423a23b2bfe7cd4baed6475ce8324
ephantus ngugi <ngugiephy@gmail.com>
2019-11-26 16:53:07 +0300

    _

--- 2_push_btn/rpi/btn_led.py
+++ 2_push_btn/rpi/btn_led.py
@@ -4,9 +4,9 @@ from time import sleep
 # set ip/op vars
 led_pin = 7
 btn_pin = 8
-btn_state = 0
+btn_state = 1
 
-GPIO.setwarnings(False)
+#GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -14,8 +14,14 @@ GPIO.setup(btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
     # read btn state
     btn_state = GPIO.input(btn_pin)
-    if btn_state == GPIO.HIGH
-        GPIO.output(led_pin, GPIO.HIGH)
-    else if btn_state == GPIO.LOW
-        GPIO.output(led_pin, GPIO.LOW)
+    print(btn_state)
+    GPIO.output(led_pin, GPIO.HIGH)
+    sleep(5)
+    GPIO.output(led_pin, GPIO.LOW)
+    sleep(1)
+ #   if btn_state == GPIO.HIGH:
+  #      GPIO.output(led_pin, GPIO.HIGH)
+#	print("btn pressed")
+ #   elif btn_state == GPIO.LOW:
+  #      GPIO.output(led_pin, GPIO.LOW)
 GPIO.cleanup()
